e5e383554bb0ce401bc590f59a956260b53584f8
Aaron Addleman <aaronaddleman@gmail.com>
2020-06-06 22:18:40 -0700

    first commit

---
+++ start.sh
@@ -0,0 +1,1 @@
+minikube start --vm-driver='docker' --kubernetes-version=v1.15.0 --memory=6g --bootstrapper=kubeadm --extra-config=kubelet.authentication-token-webhook=true --extra-config=kubelet.authorization-mode=Webhook --extra-config=scheduler.address=0.0.0.0 --extra-config=controller-manager.address=0.0.0.0
